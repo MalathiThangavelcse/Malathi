8dc9271caf07fc10d1a6e60230cea7a73bad4eab
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2017-09-10 15:33:42 +0530

    Create cuboid.c

---
+++ cuboid.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int L,B,H;
+float surfacearea,volume;
+scanf("%d %d %d",&L,&B,&H);
+surfcaearea=2*(B*L*L*H+H*B);
+volume=B*L*H;
+printf("surfacearea=%f\n volume=%f",surfacearea,volume);
+return 0;
+}
