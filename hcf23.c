69e5406ce122fa2bb1f346b2d1bd8740d6c44cd1
MalathiThangavelcse <30829797+MalathiThangavelcse@users.noreply.github.com>
2017-09-13 19:20:50 +0530

    Update hcf23.c

--- hcf23.c
+++ hcf23.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int findHcf(int a,int b)
 {
-int temp;     
+int temp;
 if(a==0 || b==0)
 return 0;
 while(b!=0)
 {
-temp = a%b;
-a= b;
-b= temp;
+temp=a%b;
+a=b;
+b=temp;
 }
 return a;
 }
